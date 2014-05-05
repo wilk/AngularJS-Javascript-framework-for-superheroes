1a7145489ed5a6a6c883604661ca25dc5905a2ab
Vincenzo (Wilk) Ferrari <wilk3ert@gmail.com>
2014-05-05 23:57:59 +0200

    added form and unit testing section draft

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -34,4 +34,17 @@ angular.module('Presentation', [])
                 msgs = [];
             }
         };
+    }])
+    .controller('FormController', ['$scope', function ($scope) {
+        $scope.master = {};
+        $scope.update = function(user) {
+            $scope.master = angular.copy(user);
+        };
+        $scope.reset = function() {
+            $scope.user = angular.copy($scope.master);
+        };
+        $scope.isUnchanged = function(user) {
+            return angular.equals(user, $scope.master);
+        };
+        $scope.reset();
     }]);
