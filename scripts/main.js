afeb7d697ac5d51c9103b2e0fced0d32f5b1ddd3
Vincenzo (Wilk) Ferrari <wilk3ert@gmail.com>
2014-05-07 17:25:01 +0200

    Generated by generator-bespoke

--- scripts/main.js
+++ scripts/main.js
@@ -8,3 +8,43 @@ bespoke.from('article', {
   state: true,
   forms: true
 });
+
+angular.module('Presentation', [])
+    .controller('DirectiveController', ['$scope', function ($scope) {
+        $scope.customer = {
+            name: 'Naomi',
+            address: '1600 Amphitheatre'
+        };
+    }])
+    .directive('myCustomer', function () {
+        return {
+            template: '<b>Name</b>: {{customer.name}} <b>Address</b>: {{customer.address}}'
+        };
+    })
+    .controller('ServiceController', ['$scope', 'notify', function ($scope, notify) {
+        $scope.notify = notify;
+    }])
+    .service('notify', ['$window', function ($window) {
+        var msgs = [];
+
+        return function (msg) {
+            msgs.push(msg);
+            if (msgs.length > 2) {
+                $window.alert(msgs);
+                msgs = [];
+            }
+        };
+    }])
+    .controller('FormController', ['$scope', function ($scope) {
+        $scope.master = {};
+        $scope.update = function (user) {
+            $scope.master = angular.copy(user);
+        };
+        $scope.reset = function () {
+            $scope.user = angular.copy($scope.master);
+        };
+        $scope.isUnchanged = function (user) {
+            return angular.equals(user, $scope.master);
+        };
+        $scope.reset();
+    }]);
